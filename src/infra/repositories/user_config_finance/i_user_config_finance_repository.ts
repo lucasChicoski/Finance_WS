9fd0feca77f37f332e8a90be7639800015dedcf7
Lucas Chicoski dos Santos <lucas.chicoski9@gmail.com>
2025-06-24 21:06:01 -0300

    finalizando o atualizar de finanças

--- src/infra/repositories/user_config_finance/i_user_config_finance_repository.ts
+++ src/infra/repositories/user_config_finance/i_user_config_finance_repository.ts
@@ -3,5 +3,5 @@ import { FinanceConfigModel } from "../../../domain/models/finance_config_model"
 export interface IUserConfigFinanceRepository {
     getFinanceConfig(userId: number): Promise<any>
     createFinanceConfig(value: FinanceConfigModel): Promise<any>
-    upateFinanceConfig(value: FinanceConfigModel): Promise<any>
+    updateFinanceConfig(value: FinanceConfigModel): Promise<any>
 }
